85c12fc1601747548fa0cf981231d5e953e51cc3
saidi aziz <user@192.168.0.17>
2019-03-29 14:44:24 +0100

    SUCCESS : should_add_Bar_if_number_divisible_by_5()

--- src/main/java/com/kata/foobarqix/serviceImpl/StringCompute.java
+++ src/main/java/com/kata/foobarqix/serviceImpl/StringCompute.java
@@ -3,10 +3,12 @@ package com.kata.foobarqix.serviceImpl;
 public class StringCompute {
 
 	public String compute(String value) {
-		
+		String output = value;
 		if (Integer.parseInt(value) % 3 == 0)
-			return "Foo";
-		return value;
+			output = "Foo";
+		if (Integer.parseInt(value) % 5 == 0)
+			output = value.concat("Bar");
+		return output;
 	}
 
 }
